5220e3cd04d287e064cc630785e2256a3c6a66df
seng-han <kumjjick@icloud.com>
2024-11-26 14:36:21 +0900

    Red

---
+++ src/test/java/com/ll/testUtilTest.java
@@ -0,0 +1,9 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class testUtilTest {
+    @Test@DisplayName("testutily.getscanner()")
+    public
+}
